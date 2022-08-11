ad234ab065d9ce053be6a01aaea95828cd96b8a1
batuschkam <110816956+batuschka@users.noreply.github.com>
2022-08-11 21:28:21 +0300

    Add files via upload

---
+++ yazılım/sitebatu.js
@@ -0,0 +1,34 @@
+// Popup Al
+var modal = document.getElementById('myModal');
+
+// Kipi açan düğmeyi al
+var btn = document.getElementById("myBtn");
+
+// Kipi kapatan <span> öğesini edinin
+var span = document.getElementsByClassName("close")[0];
+
+// Kullanıcı düğmeyi tıklattığında
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// Kullanıcı <span> (x) düğmesini tıkladığında, popup
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Kullanıcı modelden başka herhangi bir yeri tıklattıysa, onu kapatın.
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+function welcomeTheUsar() {
+    // Some code borrowed and rewritten from UNKNOWN's lessons
+
+    let firstName = document.getElementById("inputName").value; 
+    let lastName = document.getElementById("inputSurname").value;
+    let fullName = "inputName" + "inputSurname".value;
+    console.log(fullName);
+    alert("Hoşgeldin "+fullName);
+}
